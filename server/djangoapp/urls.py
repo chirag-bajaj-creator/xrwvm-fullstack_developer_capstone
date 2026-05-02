8d154c6afb0275818437ff6b7f852030a9f93a2e
chirag-bajaj-creator <chiragbajaj2203@gmail.com>
2026-05-02 02:39:00 -0400

    d

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,48 +1,7 @@
-import os
-from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
-from django.views.static import serve
-from django.conf.urls.static import static
-from django.conf import settings
-
-FRONTEND_BUILD_DIR = "/home/project/xrwvm-fullstack_developer_capstone/server/frontend/build"
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    # Backend API
-    path('djangoapp/', include('djangoapp.urls')),
-    path('admin/', admin.site.urls),
-
-    # Django pages
-    path('', TemplateView.as_view(template_name="Home.html")),
-    path('about/', TemplateView.as_view(template_name="About.html")),
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
-
-    # React pages
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    path('register/', TemplateView.as_view(template_name="index.html")),
-    path('dealers/', TemplateView.as_view(template_name="index.html")),
-
-    # React public build files
-    re_path(r'^manifest\.json$', serve, {
-        'path': 'manifest.json',
-        'document_root': FRONTEND_BUILD_DIR,
-    }),
-
-    re_path(r'^favicon\.ico$', serve, {
-        'path': 'favicon.ico',
-        'document_root': FRONTEND_BUILD_DIR,
-    }),
-
-    re_path(r'^logo192\.png$', serve, {
-        'path': 'logo192.png',
-        'document_root': FRONTEND_BUILD_DIR,
-    }),
-
-    re_path(r'^logo512\.png$', serve, {
-        'path': 'logo512.png',
-        'document_root': FRONTEND_BUILD_DIR,
-    }),
+    path('get_dealers/', views.get_dealership, name='get_dealers'),
+    path('get_dealers/<str:state>/', views.get_dealership, name='get_dealers_by_state'),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,24 +1,47 @@
-"""djangoproj URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-"""
+import os
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.views.static import serve
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [
+FRONTEND_BUILD_DIR = os.path.join(settings.BASE_DIR, "frontend", "build")
 
-    # Backend API routes
-    path('djangoapp/', include('djangoapp.urls')),
+urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    path('register/', TemplateView.as_view(template_name="index.html")),
-    # Frontend pages
+
+    # Backend API
+    path('djangoapp/', include('djangoapp.urls')),
+
+    # Django pages
     path('', TemplateView.as_view(template_name="Home.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
+
+    # React pages
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+    # React public build files
+    re_path(r'^manifest\.json$', serve, {
+        'path': 'manifest.json',
+        'document_root': FRONTEND_BUILD_DIR,
+    }),
+    re_path(r'^favicon\.ico$', serve, {
+        'path': 'favicon.ico',
+        'document_root': FRONTEND_BUILD_DIR,
+    }),
+    re_path(r'^logo192\.png$', serve, {
+        'path': 'logo192.png',
+        'document_root': FRONTEND_BUILD_DIR,
+    }),
+    re_path(r'^logo512\.png$', serve, {
+        'path': 'logo512.png',
+        'document_root': FRONTEND_BUILD_DIR,
+    }),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
